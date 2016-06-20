bfa5051936e46e5eeca2eec7b3d38ccde1394d5a
Gwindor-Aut <florian.schweighofer@edu.fh-joanneum.at>
2016-06-20 12:47:42 +0200

    Dayview, colors, bootstrap-grid, divs

--- php/renderSchedule.php
+++ php/renderSchedule.php
@@ -3,11 +3,11 @@
 function draw_calendar($month,$year,$xml){
 
 	/* draw table */
-	$calendar = '<table cellpadding="5px" cellspacing="0" class="calendar">';
+	$calendar = '<div class="container-fluid">';
 
 	/* table headings */
 	$headings = array('Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag');
-	$calendar.= '<tr class="calendar-row"><td class="calendar-day-head">'.implode('</td><td class="calendar-day-head">',$headings).'</td></tr>';
+	$calendar.= '<div class="row"><div class="col-sm-1 calendar-day-head">'.implode('</div><div class="col-sm-1 calendar-day-head">',$headings).'</div></div>';
 
 	/* days and weeks vars now ... */
 	$running_day = date('w',mktime(0,0,0,$month,1,$year));
@@ -17,17 +17,17 @@ function draw_calendar($month,$year,$xml){
 	$dates_array = array();
 
 	/* row for week one */
-	$calendar.= '<tr class="calendar-row">';
+	$calendar.= '<div class="row">';
 
 	/* print "blank" days until the first of the current week */
 	for($x = 0; $x < $running_day; $x++):
-		$calendar.= '<td class="calendar-day-np"> </td>';
+		$calendar.= '<div class="col-sm-1 calendar-day-np"> </div>';
 		$days_in_this_week++;
 	endfor;
 
 	/* keep going with days.... */
 	for($list_day = 1; $list_day <= $days_in_month; $list_day++):
-		$calendar.= '<td class="calendar-day">';
+		$calendar.= '<div class="col-sm-1 calendar-day">';
 			/* add in the day number */
 			$calendar.= '<div class="day-number">'.$list_day.'</div>';
 
@@ -35,16 +35,16 @@ function draw_calendar($month,$year,$xml){
 			$datum = Date('Y-m-d', mktime(0,0,0,$month,$list_day,$year));
 			foreach ($xml->Event as $record) {
 				if (Date( 'Y-m-d', (int)$record->Start) == $datum){
-					$calendar.= str_repeat('<div class="schedule_item">'."\n".'<p>'.$record->Title.' - '.$record->Type.'</p>'."\n". '<p>'.Date( 'H:i', (int)$record->Start).'-'.Date( 'H:i', (int)$record->End).'</p>'."\n". '</div>'."\n",1);
+					$calendar.= str_repeat('<div class="'.$record->Type.' schedule_item">'."\n".'<p>'.$record->Title.' - '.$record->Type.'</p>'."\n". '<p>'.Date( 'H:i', (int)$record->Start).'-'.Date( 'H:i', (int)$record->End).'</p>'."\n". '</div>'."\n",1);
 				}
 			}
 			$calendar.= str_repeat('<p> </p>',2);
 
-		$calendar.= '</td>';
+		$calendar.= '</div>';
 		if($running_day == 6):
-			$calendar.= '</tr>';
+			$calendar.= '</div>';
 			if(($day_counter+1) != $days_in_month):
-				$calendar.= '<tr class="calendar-row">';
+				$calendar.= '<div class="row">';
 			endif;
 			$running_day = -1;
 			$days_in_this_week = 0;
@@ -55,15 +55,15 @@ function draw_calendar($month,$year,$xml){
 	/* finish the rest of the days in the week */
 	if($days_in_this_week < 8):
 		for($x = 1; $x <= (8 - $days_in_this_week); $x++):
-			$calendar.= '<td class="calendar-day-np"> </td>';
+			$calendar.= '<div class="col-sm-1 calendar-day-np"> </div>';
 		endfor;
 	endif;
 
 	/* final row */
-	$calendar.= '</tr>';
+	$calendar.= '</div>';
 
 	/* end the table */
-	$calendar.= '</table>';
+	$calendar.= '</div>';
 
 	/* all done, return result */
 	return $calendar;
@@ -71,11 +71,11 @@ function draw_calendar($month,$year,$xml){
 function draw_week($givenday,$month,$year,$xml){
 
 	/* draw table */
-	$calendar = '<table cellpadding="5px" cellspacing="0" class="calendar">';
+	$calendar = '<div class="container-fluid">';
 
 	/* table headings */
 	$headings = array('Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag');
-	$calendar.= '<tr class="calendar-row"><td class="calendar-day-head">'.implode('</td><td class="calendar-day-head">',$headings).'</td></tr>';
+	$calendar.= '<div class="row"><div class="col-sm-1 calendar-day-head">'.implode('</div><div class="col-sm-1 calendar-day-head">',$headings).'</div></div>';
 
 	/* days and weeks vars now ... */
 	$day = date('w',mktime(0,0,0,$month,$givenday,$year));
@@ -89,12 +89,12 @@ function draw_week($givenday,$month,$year,$xml){
     $daycopy = $week_start_day;
 
 	/* row for week one */
-	$calendar.= '<tr class="calendar-row">';
+	$calendar.= '<div class="row">';
 
 
 	/* keep going with days.... */
 	for($list_day = 1; $list_day <= 7; $list_day++):
-		$calendar.= '<td class="calendar-day">';
+		$calendar.= '<div class="col-sm-1 calendar-day">';
 			/* add in the day number */
 			$calendar.= '<div class="day-number">'.$daycopy.'</div>';
 
@@ -102,7 +102,7 @@ function draw_week($givenday,$month,$year,$xml){
 			$datum = Date('Y-m-d', mktime(0,0,0,$month,$daycopy,$year));
 			foreach ($xml->Event as $record) {
 				if (Date( 'Y-m-d', (int)$record->Start) == $datum){
-					$calendar.= str_repeat('<div class="schedule_week_item">'."\n".'<p>'.$record->Title.' - '.$record->Type.'</p>'."\n".'<p>'.Date( 'H:i', (int)$record->Start).'-'.Date( 'H:i', (int)$record->End).'</p>'."\n"."\n".'<p>'.$record->Location.'</p>'."\n".'<p>'.$record->Lecturer.'</p>'."\n".'</div>',1);
+					$calendar.= str_repeat('<div class="'.$record->Type.' schedule_item">'."\n".'<p>'.$record->Title.' - '.$record->Type.'</p>'."\n".'<p>'.Date( 'H:i', (int)$record->Start).'-'.Date( 'H:i', (int)$record->End).'</p>'."\n"."\n".'<p>'.$record->Location.'</p>'."\n".'<p>'.$record->Lecturer.'</p>'."\n".'</div>',1);
 				}
 			}
         $daycopy++;
@@ -110,16 +110,44 @@ function draw_week($givenday,$month,$year,$xml){
             $daycopy = 1;
         }
 
-		$calendar.= '</td>';
+		$calendar.= '</div>';
 
 		$days_in_this_week++; $day_counter++;
 	endfor;
 
 	/* final row */
-	$calendar.= '</tr>';
+	$calendar.= '</div>';
 
 	/* end the table */
-	$calendar.= '</table>';
+	$calendar.= '</div>';
+
+	/* all done, return result */
+	return $calendar;
+}
+function draw_day($givenday,$month,$year,$xml){
+
+    $day = date('w',mktime(0,0,0,$month,$givenday,$year));
+	/* draw table */
+	$calendar = '<div class="container-fluid">';
+
+	/* table headings */
+	$headings = array('Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag');
+	$calendar.= '<div class="row"><div class="col-sm-1 calendar-day-head">'.$headings[(int)$day].'</div></div>';
+
+	/* row for week one */
+	$calendar.= '<div class="row">';
+    
+    $datum = Date('Y-m-d', mktime(0,0,0,$month,$givenday,$year));
+    foreach ($xml->Event as $record) {
+        if (Date( 'Y-m-d', (int)$record->Start) == $datum){
+            $calendar.= str_repeat('<div class="'.$record->Type.' schedule_item">'."\n".'<p>'.$record->Title.' - '.$record->Type.'</p>'."\n".'<p>'.Date( 'H:i', (int)$record->Start).'-'.Date( 'H:i', (int)$record->End).'</p>'."\n"."\n".'<p>'.$record->Location.'</p>'."\n".'<p>'.$record->Lecturer.'</p>'."\n".'</div>',1);
+        }
+    }
+    $calendar.= '</div>';
+    $calendar.= '</div>';
+
+    /* end the table */
+	$calendar.= '</div>';
 
 	/* all done, return result */
 	return $calendar;
--- schedule.php
+++ schedule.php
@@ -6,8 +6,13 @@ include ('php/getSchedule.php');
 <head>
 	<title>FH-Schedule</title>
 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/schedulestyle.css" title="default" media="screen" />
 	<link rel="icon" type="img/png" href="./img/favicon.ico">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js" defer></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" defer></script>
 </head>
 	<body>
         <?php
